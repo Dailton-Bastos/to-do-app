7ad1bfdedaf24cca57aef54cf293f5b461a69697
Dailton Bastos <dailtonbastos@gmail.com>
2023-02-09 21:01:33 -0300

    feat: add component header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import logo from '../../assets/logo.svg';
+
+import styles from './Header.module.css';
+
+export const Header = () => {
+  return (
+    <header className={styles.header}>
+      <img src={logo} alt="ToDo" />
+    </header>
+  );
+};
